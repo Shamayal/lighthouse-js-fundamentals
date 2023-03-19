f4fa025c708d876b6d34f25cb459107e64a2ff1a
Shamayal Syed <shamayalsyed9@gmail.com>
2023-03-19 16:07:32 -0400

    Practicing with Functions

--- hello.js
+++ hello.js
@@ -3,3 +3,15 @@ const sayHello  = function (name) {
 }
 
 sayHello("Shamayal");
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('Syed'); 
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('Shama');
+console.log(greeting);
+
